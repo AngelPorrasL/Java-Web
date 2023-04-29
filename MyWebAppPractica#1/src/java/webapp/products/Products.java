80f3c23846454044051edfff1195ee0178264805
Angel Porras L <99231042+AngelPorrasL@users.noreply.github.com>
2023-04-29 17:21:17 -0600

    Add files via upload

---
+++ MyWebAppPractica#1/src/java/webapp/products/Products.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package webapp.products;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Products {
+    
+    String Name;
+    Float Price;
+    
+    public Products(String pName, Float pPrice){
+        this.Name = pName;
+        this.Price = pPrice;
+    }
+}
